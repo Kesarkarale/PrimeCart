fecf5b317a931f13aa2cd00cf1ab787e135d0311
Kesar Karale <kesarkarale1@gmail.com>
2026-07-13 19:48:49 +0530

    Create page.tsx

---
+++ app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,27 @@
+import ForgotPasswordForm 
+from "@/components/auth/forgot-password-form";
+
+
+export default function ForgotPassword(){
+
+return(
+
+<div className="
+min-h-screen
+flex
+items-center
+justify-center
+bg-gradient-to-br
+from-slate-950
+via-slate-900
+to-slate-800
+px-4
+">
+
+<ForgotPasswordForm/>
+
+</div>
+
+);
+
+}
