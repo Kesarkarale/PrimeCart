4496ff38b1e43cf134830ecd2734e8b35af5c9d1
Kesar Karale <kesarkarale1@gmail.com>
2026-07-14 12:45:19 +0530

    Create product.ts

---
+++ types/product.ts
@@ -0,0 +1,21 @@
+export interface Product {
+
+id:string;
+
+name:string;
+
+description:string;
+
+price:number;
+
+old_price?:number;
+
+image:string;
+
+category:string;
+
+stock:number;
+
+rating:number;
+
+}
