378642d545ceef4d4f31285b3a3efede5897231c
Kesar Karale <kesarkarale1@gmail.com>
2026-07-14 18:08:00 +0530

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,80 +1,18 @@
- import type { ReactNode } from "react";
-
-import Sidebar from "@/components/layout/sidebar";
-import Navbar from "@/components/layout/navbar";
-
-
 export default function DashboardLayout({
-
-children,
-
+children
 }:{
-
-children:ReactNode;
-
+children: React.ReactNode
 }){
 
 
 return (
 
-<div className="
-min-h-screen
-
-bg-gray-50
-dark:bg-[#050505]
-
-text-gray-900
-dark:text-white
-
-transition-colors
-duration-300
-">
-
-
-<Sidebar />
-
-
-
-<div className="
-md:ml-72
-min-h-screen
-flex
-flex-col
-">
-
-
-
-<Navbar />
-
-
-
-<main className="
-flex-1
-
-p-5
-md:p-8
-
-bg-gray-50
-dark:bg-[#050505]
-
-transition-colors
-duration-300
-">
-
+<div className="min-h-screen">
 
 {children}
 
-
-</main>
-
-
-
 </div>
 
-
-
-</div>
-
-);
+)
 
 }
