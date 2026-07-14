45fe5bbac0b2f3f42e5f6f47cfd07cfb5aa44ef1
Kesar Karale <kesarkarale1@gmail.com>
2026-07-14 12:45:47 +0530

    Create products.ts

---
+++ lib/products.ts
@@ -0,0 +1,33 @@
+import { createClient } from "@/lib/supabase/client";
+
+
+export async function getProducts(){
+
+const supabase = createClient();
+
+
+const {data,error}=await supabase
+.from("products")
+.select("*")
+.order(
+"created_at",
+{
+ascending:false
+}
+);
+
+
+
+if(error){
+
+console.log(error);
+
+return [];
+
+}
+
+
+
+return data;
+
+}
