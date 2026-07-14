a96c7d1bfbbfc026d2657f950183ffcc8d769b9d
Kesar Karale <kesarkarale1@gmail.com>
2026-07-14 13:45:20 +0530

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from "react";
+ import type { ReactNode } from "react";
 
 import Sidebar from "@/components/layout/sidebar";
 import Navbar from "@/components/layout/navbar";
@@ -21,22 +21,19 @@ return (
 min-h-screen
 
 bg-gray-50
-text-gray-900
-
 dark:bg-[#050505]
+
+text-gray-900
 dark:text-white
 
 transition-colors
 duration-300
 ">
 
-{/* Sidebar */}
 
 <Sidebar />
 
 
-
-{/* Main Area */}
 
 <div className="
 md:ml-72
@@ -47,27 +44,23 @@ flex-col
 
 
 
-{/* Navbar */}
-
 <Navbar />
 
 
 
-{/* Page Content */}
-
 <main className="
 flex-1
+
 p-5
 md:p-8
 
 bg-gray-50
 dark:bg-[#050505]
 
-min-h-screen
-
 transition-colors
 duration-300
 ">
+
 
 {children}
 
@@ -83,6 +76,5 @@ duration-300
 </div>
 
 );
-
 
 }
