7b2ef55c345b457e1298a2efc340014be90d5cb1
Kesar Karale <kesarkarale1@gmail.com>
2026-07-22 19:32:19 +0530

    Update hero.tsx

--- components/dashboard/hero.tsx
+++ components/dashboard/hero.tsx
@@ -2,18 +2,38 @@
 
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const banners = [
-  "/banner/hero-banner.png",
-  "/banner/hero-banner2.png",
-  "/banner/hero-banner3.png",
-  "/banner/hero-banner4.png",
-  "/banner/hero-banner5.png",
-  "/banner/hero-banner6.png",
+  {
+    image: "/banner/hero-banner.png",
+    link: "/categories/fashion",
+  },
+  {
+    image: "/banner/hero-banner2.png",
+    link: "/categories/electronics",
+  },
+  {
+    image: "/banner/hero-banner3.png",
+    link: "/categories/home-living",
+  },
+  {
+    image: "/banner/hero-banner4.png",
+    link: "/categories/beauty",
+  },
+  {
+    image: "/banner/hero-banner5.png",
+    link: "/categories/fashion",
+  },
+  {
+    image: "/banner/hero-banner6.png",
+    link: "/categories/electronics",
+  },
 ];
 
 export default function Hero() {
   const [current, setCurrent] = useState(0);
+  const router = useRouter();
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -25,22 +45,24 @@ export default function Hero() {
 
   return (
     <section className="w-full">
-<div className="relative h-[180px] sm:h-[240px] md:h-[300px] lg:h-[360px] xl:h-[400px] rounded-[30px] overflow-hidden shadow-xl">
-  {banners.map((banner, index) => (
+      <div className="relative h-[180px] sm:h-[240px] md:h-[300px] lg:h-[360px] xl:h-[400px] rounded-[30px] overflow-hidden shadow-xl">
+        
+        {banners.map((banner, index) => (
           <Image
-            key={banner}
-            src={banner}
+            key={banner.image}
+            src={banner.image}
             alt={`Banner ${index + 1}`}
             fill
             priority={index === 0}
-            className={`object-cover transition-opacity duration-700 ${
+            onClick={() => router.push(banner.link)}
+            className={`object-cover cursor-pointer transition-opacity duration-700 ${
               current === index ? "opacity-100" : "opacity-0"
             }`}
           />
         ))}
 
         {/* Dots */}
-        <div className="absolute bottom-5 left-1/2 -translate-x-1/2 flex gap-2">
+        <div className="absolute bottom-5 left-1/2 -translate-x-1/2 flex gap-2 z-10">
           {banners.map((_, index) => (
             <button
               key={index}
